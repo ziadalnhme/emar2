2f4be0b5d8119ab263096555cec2945ff5097440
ziadalnhme <69083720+ziadalnhme@users.noreply.github.com>
2025-08-26 23:43:14 +0300

    إنشاء صفحة منفصلة للشهادات

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
-import Certificates from './components/Certificates';
 import Clients from './components/Clients';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
@@ -16,7 +15,6 @@ function App() {
       <Hero />
       <About />
       <Services />
-      <Certificates />
       <Clients />
       <Projects />
       <Contact />
