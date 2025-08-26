55f43ea69baa99705482da502b1da2f8229eecab
ziadalnhme <69083720+ziadalnhme@users.noreply.github.com>
2025-08-26 23:44:04 +0300

    إصلاح التنقل للشهادات

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero';
 import About from './components/About';
 import Services from './components/Services';
 import Clients from './components/Clients';
+import Certificates from './components/Certificates';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Services />
       <Clients />
+      <Certificates />
       <Projects />
       <Contact />
       <Footer />
